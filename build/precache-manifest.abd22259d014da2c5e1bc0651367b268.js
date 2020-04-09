c76f891117a8f92a989557b55ce5d14782b47040
trion bailey <trionbailey@gmail.com>
2020-04-09 15:05:12 -0400

    updated route

---
+++ build/precache-manifest.abd22259d014da2c5e1bc0651367b268.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9f351bfd4121d2931414679601e9f8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0fd52e256041d2ecbfd7",
+    "url": "/react-portfolio/static/css/main.885875be.chunk.css"
+  },
+  {
+    "revision": "3f687c0d6f5ad89b1e92",
+    "url": "/react-portfolio/static/js/2.68f5bd4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.68f5bd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd52e256041d2ecbfd7",
+    "url": "/react-portfolio/static/js/main.bdc8f4f0.chunk.js"
+  },
+  {
+    "revision": "c6c76a20bc1ada822d34",
+    "url": "/react-portfolio/static/js/runtime-main.084a3d78.js"
+  },
+  {
+    "revision": "82f9a3f40cbf7e9e8d81f8a2b24f9418",
+    "url": "/react-portfolio/static/media/EatDatSushi.82f9a3f4.png"
+  },
+  {
+    "revision": "389d63c732726a58de63892bc7630967",
+    "url": "/react-portfolio/static/media/Employee Generator.389d63c7.png"
+  },
+  {
+    "revision": "f29e0f69f5f53e46a1aa4ee9721a3e08",
+    "url": "/react-portfolio/static/media/cooldude.f29e0f69.jpeg"
+  },
+  {
+    "revision": "6d0a3eb99618b6ca58dc98e63df2e829",
+    "url": "/react-portfolio/static/media/fyre.6d0a3eb9.png"
+  },
+  {
+    "revision": "1d6d7d3e36887af743a76b323b524e88",
+    "url": "/react-portfolio/static/media/note-taker.1d6d7d3e.png"
+  },
+  {
+    "revision": "615300605ab41e482eb114a491c9e6ac",
+    "url": "/react-portfolio/static/media/passwordgenerator.61530060.png"
+  },
+  {
+    "revision": "72403ae07a457b9e0dc364c6fac34a08",
+    "url": "/react-portfolio/static/media/soundtrackmovie.72403ae0.png"
+  }
+]);
