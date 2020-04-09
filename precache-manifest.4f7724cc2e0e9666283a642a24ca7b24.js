6b60674182f4e7c2315f8f5bb68238a98ee46911
trion bailey <trionbailey@gmail.com>
2020-04-09 09:38:28 -0400

    Updates

---
+++ precache-manifest.4f7724cc2e0e9666283a642a24ca7b24.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2716d28902723a7dda2857e1585c504f",
+    "url": "/trionb/react-portfolio/index.html"
+  },
+  {
+    "revision": "ba119b213078eaba83fe",
+    "url": "/trionb/react-portfolio/static/css/main.885875be.chunk.css"
+  },
+  {
+    "revision": "d9ad0a6f2db003756dce",
+    "url": "/trionb/react-portfolio/static/js/2.3d454b50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trionb/react-portfolio/static/js/2.3d454b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba119b213078eaba83fe",
+    "url": "/trionb/react-portfolio/static/js/main.74c6b3ab.chunk.js"
+  },
+  {
+    "revision": "6f311b7dd43ac111244f",
+    "url": "/trionb/react-portfolio/static/js/runtime-main.5d89f79c.js"
+  },
+  {
+    "revision": "3453c99d0627229a63a24b763bd5cc20",
+    "url": "/trionb/react-portfolio/static/media/Eat-Da-Sushi.3453c99d.png"
+  },
+  {
+    "revision": "389d63c732726a58de63892bc7630967",
+    "url": "/trionb/react-portfolio/static/media/Employee Generator.389d63c7.png"
+  },
+  {
+    "revision": "6d0a3eb99618b6ca58dc98e63df2e829",
+    "url": "/trionb/react-portfolio/static/media/fyre.6d0a3eb9.png"
+  },
+  {
+    "revision": "1d6d7d3e36887af743a76b323b524e88",
+    "url": "/trionb/react-portfolio/static/media/note-taker.1d6d7d3e.png"
+  },
+  {
+    "revision": "615300605ab41e482eb114a491c9e6ac",
+    "url": "/trionb/react-portfolio/static/media/passwordgenerator.61530060.png"
+  },
+  {
+    "revision": "72403ae07a457b9e0dc364c6fac34a08",
+    "url": "/trionb/react-portfolio/static/media/soundtrackmovie.72403ae0.png"
+  }
+]);
