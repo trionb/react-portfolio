4bca10a12dfcec1e69180771f3b24f76258798f0
trion bailey <trionbailey@gmail.com>
2020-05-10 22:02:04 -0400

    updated about me page

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -18,8 +18,7 @@ function AboutMe() {
                                             <p><strong>"A smooth sea never made a skilled sailor"-FDR.</strong></p>
                                             <p> HELLO WORLD!</p>
                                             <p>
-                                            I'm a currently learning full-stack development. I am for Maryland but moved to New Jersey.
-                                            Taking something from thought and being able to see it come to life is the reason I enjoy learning and developing projects.
+                                            I'm a currently learning full-stack development. I am Trion Bailey and I am from Maryland but moved to New Jersey. Taking something from thought and being able to see it come to life is the reason I enjoy learning and developing projects. Since graduating from Penn bootcamp I have developed the skills to efficiently communicate in a group setting in order to successfully complete and present a project along with being equipped to work remotely.
                                           </p>
                                         </div>
                                  </section>
